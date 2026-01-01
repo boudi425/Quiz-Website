b275fca6fdff74f0f43634d1a2671da73f1f15b0
boudi425 <bodybrkt@gmail.com>
2026-01-01 16:54:11 +0300

    The Project Starts!

---
+++ my-app/src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <h1 className="text-blue-300 font-bold underline">Hello, World</h1>
+)
