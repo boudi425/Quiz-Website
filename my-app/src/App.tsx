0c245535c88b6f14797ed95511735843c2c79261
boudi425 <bodybrkt@gmail.com>
2026-01-03 21:02:29 +0300

    Set up Completed

---
+++ my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import App from "./pages/signUp.tsx"
+
+export default function App() {
+    return (
+        <main className="flex items-center justify-center md:p-6 lg:p-0">
+            <h1 className="font-heading font-bold">Hello</h1>
+            <button className="btn-primary font-sans">Click Me</button>
+        </main>
+    )
+}
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-
+import App from "./App.tsx"
+import "./index.css"
 createRoot(document.getElementById('root')!).render(
-  <h1 className="text-blue-300 font-bold underline">Hello, World</h1>
+  <App />
 )
